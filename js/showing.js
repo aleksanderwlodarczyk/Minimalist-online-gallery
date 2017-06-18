ef9db782893a0ea2eb5ebb8ed33deec7ac902258
Aleksander Włodarczyk <aleksander.wlodarczyk1@gmail.com>
2017-06-18 23:05:40 +0200

    contact form

--- js/showing.js
+++ js/showing.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 	console.log("got it!");
 
 	var clicked = false;
+	var menu_vis = false;
 
 	$(".image").mouseenter(function(){
 		//$(this).width($(this).width()+10);
@@ -21,15 +22,24 @@ $(document).ready(function(){
 	$(".gitem").on("click", function(){
 		if(!clicked){
 			$(this).addClass("show");
-			$('html, body').animate({scrollTop: ($(this).offset().top-20)},550);
+			$('html, body').animate({scrollTop: ($(this).offset().top)},550);
 			clicked = true;
 		}
 		else {
 			$(this).removeClass("show");
 			clicked = false;
 		}
-
-		
 	});
+
+	
+	$(".menu_link").on("click", function(){
+		//chowanie menu po kliknieciu gdziekolwiek na stronie
+		 $(".side_menu").toggleClass("side_menuA");
+
+	});
+
+	
+	
+
 
 });
